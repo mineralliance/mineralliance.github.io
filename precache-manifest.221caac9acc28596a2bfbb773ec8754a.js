9827cef5fafaf033b000720bef4dd67953d3880a
DeFiDev <defidev2021@gmail.com>
2021-11-03 03:37:00 -0700

    test

---
+++ precache-manifest.221caac9acc28596a2bfbb773ec8754a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d031232e60c02f09d798",
+    "url": "/static/css/main.7a36aa56.chunk.css"
+  },
+  {
+    "revision": "d031232e60c02f09d798",
+    "url": "/static/js/main.d031232e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "a5ce1ee87d72994e0ddd",
+    "url": "/static/css/2.fc0b62e8.chunk.css"
+  },
+  {
+    "revision": "a5ce1ee87d72994e0ddd",
+    "url": "/static/js/2.a5ce1ee8.chunk.js"
+  },
+  {
+    "revision": "dd46996e08d7a052d237be2254405fda",
+    "url": "/static/media/logo.dd46996e.png"
+  },
+  {
+    "revision": "accfa4e9d76fd84404c2d635e43cad97",
+    "url": "/static/media/bg.accfa4e9.jpg"
+  },
+  {
+    "revision": "75b2f160920d6f305972996110881bd5",
+    "url": "/index.html"
+  }
+];
